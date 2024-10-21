cd6bc03e20bb08046d232abbf2c543909d634614
SahakyanArtur219 <arthursahakyan.tt219-1@polytecnic>
2024-10-21 23:07:35 +0400

    add SSA

---
+++ SimpleSubstitutionAlgorithm/SimpleSubstitutionAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+
+const char* alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+void encrypt(char text[], const char alphabet[]) {
+    for (int i = 0; i < strlen(text); ++i) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = alphabet[text[i] - 'A' + 26];
+        }
+        else if (text[i] >= 'a' && text[i] <= 'z') {
+            text[i] = alphabet[text[i] - 'a'];
+        }
+    }
+}
+
+int main() {
+
+    char text[100];
+
+    std::cout << "Enter text to encrypt: ";
+    std::cin.getline(text, 100);
+
+    encrypt(text, alphabet);
+
+    std::cout << "Encrypted text: " << text << std::endl;
+
+    return 0;
+}
