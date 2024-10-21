a6c339f840aead88d60bb11a50e7b08b2ac9bea6
SahakyanArtur219 <arthursahakyan.tt219-1@polytecnic>
2024-10-21 22:46:21 +0400

    Change GCD and LCM

--- GreatestCommonDivisor/GreatestCommonDivisor.cpp
+++ GreatestCommonDivisor/GreatestCommonDivisor.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void GCD(int a, int b){
+
+    if (a < 1 || b < 1) {
+        cout << "Please input correct numbers\n";
+        return;
+    }
+
+    if (b > a) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+
+    for (int i = b; i >= 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            cout << "The number is = " << i << endl << endl;
+            break;
+        }
+    }
+}
+
 int main() {
 
     cout << "Greatest Common Divisor" << endl << endl;
@@ -13,24 +34,8 @@ int main() {
 
         cout << " a = "; cin >> a; cout << endl;
         cout << " b = "; cin >> b; cout << endl;
+        GCD(a, b);
 
-        if (a > b) {
-
-            for (i = b; i >= 1; i--) {
-                if (a % i == 0 && b % i == 0) {
-                    cout << "The number is = " << i << endl << endl;
-                    break;
-                }
-            }
-        }
-        else {
-            for (i = a; i >= 1; i--) {
-                if (b % i == 0 && a % i == 0) {
-                    cout << "The number is = " << i << endl << endl;
-                    break;
-                }
-            }
-        }
 
     }
 
--- LeastCommonMultiple/LeastCommonMultiple.cpp
+++ LeastCommonMultiple/LeastCommonMultiple.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+
+void LCM(int a,int b) {
+
+    if (a < 1 || b < 1) {
+        cout << "Please input correct numbers\n";
+        return;
+    }
+
+    if (b > a) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+
+    for (int i = b; i <= a * b; i = i + b) {
+        if (i % a == 0 && i % b == 0) {
+            cout << "The number is = " << i << endl;
+            break;
+        }
+    }
+}
+
+
+
 int main() {
 
     cout << "Least Common Multiple" << endl << endl;
@@ -13,25 +37,8 @@ int main() {
 
         cout << " a = "; cin >> a; cout << endl;
         cout << " b = "; cin >> b; cout << endl;
-
-        if (a > b) {
-
-            for (i = b; i <= a * b; i = i + b) {
-                if (i % a == 0 && i % b == 0) {
-                    cout << "The number is = " << i << endl;
-                    break;
-                }
-            }
-        }
-        else {
-            for (i = a; i <= a * b; i = i + a) {
-                if (i % a == 0 && i % b == 0) {
-                    cout << "The number is = " << i << endl;
-                    break;
-                }
-            }
-        }
-
+        
+        LCM(a, b);
     }
 
     return 0;
